a5bc8169baad0f7b7d50e4e93a411618c540de34
Fiido <roccobosio@gmail.com>
2024-02-22 09:31:27 -0300

    add certificates

---
+++ src/data/certificates.js
@@ -0,0 +1,20 @@
+export const certificates = [
+  {
+    title: "JavaScript 2020",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    image: "/certificates/js2020.jpg",
+  },
+  {
+    title: "JavaScript 2020",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    image: "/certificates/go2021.jpg",
+  },
+  {
+    title: "JavaScript 2020",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    image: "/certificates/docker.jpg",
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    title: "HealthTech",
+    subtitle: "Consultoria medica online",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    images: [
+      "/projects/healthtech/login.png",
+      "/projects/healthtech/config.png",
+      "/projects/healthtech/doctors.png",
+      "/projects/healthtech/meetings.png",
+      "/projects/healthtech/videocall.png",
+    ],
+  },
+  {
+    title: "Hotelmar",
+    subtitle: "Reservas de habitaciones para un hotel",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    images: [
+      "/projects/hotelmar/home.png",
+      "/projects/hotelmar/booking.png",
+      "/projects/hotelmar/extras.png",
+      "/projects/hotelmar/detail.png",
+      "/projects/hotelmar/my-bookings.png",
+    ],
+  },
+  {
+    title: "GenXDB",
+    subtitle: "Generador de codigo para ORMs",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
+    images: ["/projects/genxdb/logo.png"],
+  },
+];
