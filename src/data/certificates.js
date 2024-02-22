1b1cce03167152f01087b0f528797bac86de5bf6
Fiido <roccobosio@gmail.com>
2024-02-22 12:02:50 -0300

    change icons titles and code refactoring

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,18 +1,18 @@
 export const certificates = [
   {
-    title: "JavaScript 2020",
+    title: "JavaScript - Udemy",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
     image: "/certificates/js2020.jpg",
   },
   {
-    title: "JavaScript 2020",
+    title: "Golang - Udemy",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
     image: "/certificates/go2021.jpg",
   },
   {
-    title: "JavaScript 2020",
+    title: "Docker - Udemy",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis doloribus suscipit reprehenderit illo eius optio possimus et aspernatur a quibusdam accusamus inventore voluptates rerum harum soluta, distinctio neque dignissimos!",
     image: "/certificates/docker.jpg",
